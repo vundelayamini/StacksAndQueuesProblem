30c6250cf16ba6ecdd1c3d1866fbec355ac5689b
vundelayamini <vundelayamini@gmail.com>
2021-03-24 19:05:45 +0530

    [yamini]ADD: Create A Stack

---
+++ CreateAStack/Stack.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CreateAStack
+{
+    class Stack
+    {
+
+        private Node top;
+        public Stack()
+        {
+            this.top = null;
+        }
+        public void push(int item)
+        {
+            Node node = new Node(item);
+            if (top == null)
+            {
+                top = node;
+            }
+            else
+            {
+                node.next = top;
+                top = node;
+            }
+        }
+        public void Display()
+        {
+            if (top == null)
+            {
+                Console.WriteLine("Stack is Empty");
+            }
+            else
+            {
+                Node temp = top;
+                while (temp != null)
+                {
+                    Console.Write(temp.data + " ");
+                    temp = temp.next;
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
